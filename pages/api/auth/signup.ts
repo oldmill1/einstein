317989087b212445649c30bd3839591978951c3d
Ankur Taxali <ataxali@gmail.com>
2022-09-21 11:18:53 -0400

    Add some comments

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -10,22 +10,27 @@ export default validateEmailAndPassword(async function signupHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // By the time you get email here, it is already validated
   const email = get("body.email", req)
   // Search for this email in the database
   // If it exists, return a 400
-  // Get the name and validate it using zod schema
+  // Todo: Do that ^
+  // Get the name and validate it using the zod schema
   const name = get("body.name", req)
   const nameSchema = z.string().min(1)
   try {
     nameSchema.parse(name)
     // If the name is valid proceed with storing the hash
-    // into the database. First grab the hash from the request
+    // into the database.
+    // 1. Grab the user's password from their request
     const plaintextPassword = get("body.plaintextPassword", req)
-    // then hash it with a salt value of 10
+    // 2. `hash` it with a salt value of 10
+    // (Could go up to 12 potentially in production)
     return hash(plaintextPassword, 10).then(async function (hash) {
       if (hash) {
-        // Prepare to save a new user into the database
-        // Create an object adhering to schema.prisma
+        // If the password was successfully hashed,
+        // prepare to create a new user into the database
+        // 1. Create an object adhering to schema.prisma
         const create = {
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -33,9 +38,11 @@ export default validateEmailAndPassword(async function signupHandler(
           name,
           password: hash,
         }
+        // 2. Insert using the primaClient() defined above
         const newUserCreated = await prisma.user.create({
           data: create,
         })
+        // 3. Return the response returned from prisma
         return res.status(200).send(newUserCreated)
       } // Else
       return res.status(400).send({ message: "An error occurred." })
