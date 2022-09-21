755f89bb22abdc0acb5406015c1f0df2331d5ecc
Ankur Taxali <ataxali@gmail.com>
2022-09-20 22:08:17 -0400

    Rockin it

--- tests/signup.test.ts
+++ tests/signup.test.ts
@@ -4,12 +4,13 @@ import get from "lodash/fp/get"
 
 describe("Users", function () {
   test("Signs a user up successfully.", async function () {
-    // Mimic signing up for a new account
+    // Mimic hitting the signup API handler
+    // and creating a new user named Sophie.
     const signup = postman({
       method: "POST",
       body: {
         name: "Sophie",
-        email: "sophie@bailacon.com",
+        email: "sophie@gmail.com",
         plaintextPassword: "password",
       },
     })
@@ -18,6 +19,11 @@ describe("Users", function () {
     await signupHandler(req, res)
     const data = res._getData()
     const id = get("id", data)
+    const email = get("email", data)
     expect(id).toBeDefined()
+    expect(email).toBe("sophie@gmail.com")
+    // Note: We could add mock fetch where we
+    // get the newly created user by id using
+    // the handler defined inside user/[id].ts
   })
 })
